0c3ed5a0c43634ea929c09d33f30ee92fe6ad75c
Kamil <xkamilwrobel@gmail.com>
2020-01-12 20:38:55 +0100

    Fix app

--- src/main/java/com/example/project1/Application.java
+++ src/main/java/com/example/project1/Application.java
@@ -2,6 +2,6 @@ package com.example.project1;
 
 public class Application {
     public static void main(String[] args) {
-        System.out.println("Welcome to application");
+        System.out.println("Welcome to application 1");
     }
 }
